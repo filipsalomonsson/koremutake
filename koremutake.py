e934e1cd580342b533c8f5fca10faf8040ebf2d1
filip <filip@epub-adm-08>
2006-06-11 02:23:26 +0200

    Added koremutake.py stub.

---
+++ koremutake.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import unittest
+
+__author__ = "Filip Salomonsson (filip@infix.se)"
+__date__ = "2006-06-11"
+
+
+
+class TestKoremutake(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
